16bb4915a2fa7c8564d6d635701d7b75bacb57ba
skyfire222 <skyfire222@users.noreply.github.com>
2021-12-21 20:12:54 -0800

    spear working

--- Player/PlayerStates/Walking.gd
+++ Player/PlayerStates/Walking.gd
@@ -4,6 +4,7 @@ var spearscene = preload("res://Spear/Spear.tscn")
 
 
 func enter(_msg := {}) -> void:
+	
 	pass
 
 var spear_position = Vector2(0,0)
@@ -23,10 +24,6 @@ func physics_update(_delta: float) -> void:
 		var damp_speed = clamp(player.decelerate_speed,0, abs(player.velocity.x)) # calculate magnitude of deceleration
 		player.velocity.x -= damp_speed * sign(player.velocity.x) # magnitude times direction = acceleration, then add acceleration to velocity
 	
-	var new_spear_position = (player.get_global_mouse_position() - player.global_position).normalized() * 7
-	new_spear_position.y += 4
-	
-	player.get_node("Spear").position = lerp(player.get_node("Spear").position, new_spear_position, 0.1)
 	
 	
 	
@@ -45,14 +42,35 @@ func physics_update(_delta: float) -> void:
 	# apply velocity
 	player.move_and_slide(player.velocity, player.up_direction)
 	
+	if player.get_node_or_null("Spear") != null:
+		var new_spear_position = (player.get_global_mouse_position() - player.global_position).normalized() * 7
+		new_spear_position.y += 4
+		player.get_node("Spear").position = lerp(player.get_node("Spear").position, new_spear_position, 0.1)
+	else:
+		var new_spear_position = (player.get_global_mouse_position() - player.global_position).normalized() * 3
+		new_spear_position.y += 4
+		player.get_node("nose2").position = lerp(player.get_node("nose2").position, Vector2(5,-2) + new_spear_position, 0.1)
 	
-	if (Input.is_action_pressed("key_throw")):
-		var myspear = spearscene.instance()
-		myspear.position = player.position
-		myspear.velocity = (player.get_global_mouse_position() - player.position).normalized() * player.throw_power
-		myspear.thrown = true
-		myspear.rotation = player.get_node("Spear").rotation 
-		myspear.global_position = player.get_node("Spear").global_position
-		get_tree().root.add_child(myspear)
+	
+	
+	if (Input.is_action_pressed("key_throw")) and player.get_node_or_null("Spear") != null:
+		if !(player.get_node("Timer").time_left > 0):
+			var myspear = spearscene.instance()
+			myspear.position = player.position
+			myspear.velocity = (player.get_global_mouse_position() - player.position).normalized() * player.throw_power
+			myspear.thrown = true
+			myspear.rotation = player.get_node("Spear").rotation 
+			myspear.global_position = player.get_node("Spear").global_position
+			get_tree().root.add_child(myspear)
+			player.get_node("Spear").queue_free()
+			player.get_node("Timer").start()
 		
-	
+	elif Input.is_action_pressed("key_throw"):
+		for thrown_spear in player.get_node("pickuprange").get_overlapping_bodies():
+			if (player.get_node_or_null("Spear") == null) and thrown_spear.collided:
+				thrown_spear.queue_free()
+				var myspear = spearscene.instance()
+				myspear.name = "Spear"
+				myspear.thrown = false
+				player.add_child(myspear)
+				player.get_node("Timer").start()
